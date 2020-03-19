ae2c92bd754466d5b6f3b9c7725d0165e6ca7f8e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2020-03-19 11:48:27 +0900

    Update shaders/shadertoy/download.py (Emit render pass's name/input info)

--- shaders/shadertoy/download.py
+++ shaders/shadertoy/download.py
@@ -11,10 +11,16 @@ TEMPLATE_HEADER = """\
 TEMPLATE_RENDERPASS = """
 //
 // RenderPass {i}
+// - name: {name}
+// - inputs: {inputs}
 //
 
 {code}
 """
+
+BUFFER_NAME_MAP = dict(
+    buffer00='Buffer A', buffer01='Buffer B', buffer02='Buffer C', buffer03='Buffer D')
+
 
 #
 # ShaderToy's unofficial/internal API endpoint
@@ -34,6 +40,27 @@ def scrape(id):
   return json.loads(resp.text)
 
 
+def emit_shader_pass(i, shader_pass): # (int, dict) -> str
+  code = shader_pass['code']
+  name = shader_pass['name']
+  #
+  # NOTE: Reverse engineering "input" (i.e. iChannel, Buffer A/B/C/D etc...)
+  # - 'filepath' (e.g. '/media/previz/buffer00.png') indicates Buffer A/B/C/D
+  # - 'channel' indicates associated iChennel<N> in the render pass.
+  # - For now this only handles Buffer A/B/C/D (so, e.g. static texture is not handled yet.)
+  #
+  for inp in shader_pass['inputs']:
+    if not inp['channel'] in [0, 1, 2, 3]:
+      print(f"[download:warning] shader pass ({name}) : input channel {inp['channel']} found")
+    if not inp['filepath'][14:22] in ['buffer00', 'buffer01', 'buffer02', 'buffer03']:
+      print(f"[download:warning] shader pass ({name}) : input filepath {inp['filepath']} found")
+  inputs = sorted(shader_pass['inputs'], key=lambda inp: inp['channel'])
+  input_names = [BUFFER_NAME_MAP.get(inp['filepath'][14:22], 'Unknown') for inp in inputs]
+  result = TEMPLATE_RENDERPASS.format(
+      i=i, code=code, name=name, inputs=input_names)
+  return result
+
+
 def download(id, out_dir):
   import json, re, os
 
@@ -45,14 +72,14 @@ def download(id, out_dir):
   assert resp and \
          resp[0]['info'] and \
          resp[0]['renderpass']
-  shader_info   = resp[0]['info']                                  # dict
-  shader_passes = [data['code'] for data in resp[0]['renderpass']] # [str]
+  shader_info   = resp[0]['info']       # dict
+  shader_passes = resp[0]['renderpass'] # [dict]
 
   result = ''
   view_url = f"https://www.shadertoy.com/view/{id}"
   result += TEMPLATE_HEADER.format(view_url=view_url, info=json.dumps(shader_info, indent=2))
-  for i, code in enumerate(shader_passes):
-    result += TEMPLATE_RENDERPASS.format(i=i, code=code)
+  for i, shader_pass in enumerate(shader_passes):
+    result += emit_shader_pass(i, shader_pass)
 
   name = shader_info['name']
   simple_name = re.sub('[^0-9a-z]', '_', name.lower())
